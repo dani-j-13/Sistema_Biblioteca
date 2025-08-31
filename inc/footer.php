3908b7f3086aab9ea5710c30033c47ffd5be31e4
dani-j-13 <danijua973@gmail.com>
2025-08-31 01:39:41 -0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer full-reset">
+    <div class="container-fluid">
+        
+    </div>
+    <div class="footer-copyright full-reset all-tittles">&copy; <?php echo date("Y"); ?> todos los derechos reservados</div>
+</footer>
